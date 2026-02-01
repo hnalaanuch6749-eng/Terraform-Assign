7c811af92a733d6987deab08be0503ecf92153c4
hnalaanuch6749-eng <hnalaanuch6749@gmail.com>
2026-01-31 22:35:47 -0500

    Create provider.tf

---
+++ Terraform/deploy-modules/provider.tf
@@ -0,0 +1,14 @@
+#Terraform provider s
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
